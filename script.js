f0f10bf0cf9ca65838e940e7682244ac7e02aee8
Dgbala <diogoaddelima107@gmail.com>
2024-07-25 20:24:02 -0300

    finalizado

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // manipulando o input amount para receber apenas numeros.
 amount.addEventListener("input", () => {
@@ -40,10 +41,29 @@ function convertCurrency(amount, price, symbol) {
     // Exibindo a cotação da moeda selecionada.
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`  
 
+    // Calcula o total.
+    let total = amount * price
+
+    // Verifica se o resultado não é número.
+    if (isNaN (total)) {
+      return alert ("Por favor, digite o valor corretamente para converter.")
+    }
+
+    // Formata o valor total.
+    total = formatCurrencyBRL(total).replace("r$", "")
+
+    // Exibe o rsultado total.
+    result.textContent = `${total} Reais`
+
+
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
-  } catch (error) {                     
+  } catch (error) {     
+    // Remover a classe do footer removandoo ele da tela.
+    footer.classList.remove("show-result")
+
     console.log(error);
+    alert("Não foi possível converter. tente novamente mais tarde.")
   }
 }
 
